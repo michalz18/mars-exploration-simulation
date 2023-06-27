2f53d166ffd9d9db5d8d8b16fe60ed8e813f422a
marcinlewinski <marcinlewinski71@gmail.com>
2023-06-27 14:37:28 +0200

    Moving to destination was covered with unit tests

---
+++ src/test/java/com/codecool/marsexploration/model/rovers/rovermovement/MovingToADestinationCoordinateTest.java
@@ -0,0 +1,47 @@
+package com.codecool.marsexploration.model.rovers.rovermovement;
+
+import com.codecool.marsexploration.calculators.model.Coordinate;
+import com.codecool.marsexploration.model.rovers.Rover;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class MovingToADestinationCoordinateTest {
+    MovingToADestinationCoordinate movingToADestinationCoordinate;
+    Rover rover;
+
+    @BeforeEach
+    void setUp() {
+        rover = mock(Rover.class);
+        when(rover.getCurrentPosition()).thenReturn(new Coordinate(1, 1));
+        when(rover.getDestination()).thenReturn(new Coordinate(5, 5));
+        movingToADestinationCoordinate = new MovingToADestinationCoordinate();
+    }
+
+    @Test
+    public void decideWhereToGoShouldReturnNewCoordinate() {
+        Coordinate expected = new Coordinate(2, 2);
+
+
+        Coordinate actual = movingToADestinationCoordinate.decideWhereToGo(rover);
+
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void decideWhereToGoShouldReturnCurrentPositionIfDestinationEqualsCurrent() {
+        when(rover.getDestination()).thenReturn(new Coordinate(1, 1));
+        Coordinate expected = new Coordinate(1, 1);
+
+
+        Coordinate actual = movingToADestinationCoordinate.decideWhereToGo(rover);
+
+
+        assertEquals(expected, actual);
+
+    }
+}
