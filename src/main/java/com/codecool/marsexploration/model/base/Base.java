bc9a8f12ffd37357cf27b8b24c93c8745c50202e
marcinlewinski <marcinlewinski71@gmail.com>
2023-06-29 09:09:05 +0200

    PlanBase class has been implemented

--- src/main/java/com/codecool/marsexploration/model/base/Base.java
+++ src/main/java/com/codecool/marsexploration/model/base/Base.java
@@ -40,6 +40,10 @@ public class Base {
         this.position = position;
     }
 
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     public Long getId() {
         return id;
     }
